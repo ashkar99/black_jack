2844fa41bff87b675634a716ad9dc7bf869087d0
Sadek Alashkar <sa225sd@student.lnu.se>
2024-11-10 17:37:49 +0100

    implemnt choices

--- app/src/main/java/view/EnglishView.java
+++ app/src/main/java/view/EnglishView.java
@@ -18,7 +18,7 @@ public class EnglishView implements View {
 
   /**
    * Returns pressed characters from the keyboard.
-
+   * 
    * @return the pressed character.
    */
   public int getInput() {
@@ -32,6 +32,27 @@ public class EnglishView implements View {
       System.out.println("" + e);
       return 0;
     }
+  }
+
+  /**
+   * Avilable choices.
+   */
+  public Choice choices() {
+    int input = getInput();
+    if (input == 'p') {
+      return Choice.Play;
+    }
+    if (input == 'h') {
+      return Choice.Hit;
+    }
+    if (input == 's') {
+      return Choice.Stand;
+    }
+    if (input == 'q') {
+      return Choice.Quit;
+    }
+    return null;
+
   }
 
   public void displayCard(model.Card card) {
@@ -57,7 +78,7 @@ public class EnglishView implements View {
 
   /**
    * Displays the winner of the game.
-
+   * 
    * @param dealerIsWinner True if the dealer is the winner.
    */
   public void displayGameOver(boolean dealerIsWinner) {
