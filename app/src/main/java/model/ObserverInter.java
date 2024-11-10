54648b0009d320d742365710997fc09f81d217ec
Sadek Alashkar <sa225sd@student.lnu.se>
2024-11-10 19:20:22 +0100

    fix checkstyle errors

--- app/src/main/java/model/ObserverInter.java
+++ app/src/main/java/model/ObserverInter.java
@@ -4,6 +4,14 @@ package model;
  * Observer interface.
  */
 public interface ObserverInter {
+
+  /**
+   * NOtify player that he took new card.
+   */
   public void notifyPlayerNewCard();
+
+  /**
+   * NOtify dealer that he took new card.
+   */
   public void notifyDealerNewCard();
 }
